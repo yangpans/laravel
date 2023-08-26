9b42866ee2be40a21b504513ab03c23407c4474c
yangpan <364502027@qq.com>
2023-08-26 16:44:52 +0800

    日志根据每天分表

--- app/Jobs/WriteErrorLogJob.php
+++ app/Jobs/WriteErrorLogJob.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Schema;
 
 class WriteErrorLogJob implements ShouldQueue
 {
@@ -29,6 +31,22 @@ class WriteErrorLogJob implements ShouldQueue
      */
     public function handle(): void
     {
-        DB::table('error_log')->insert($this->data);
+        $tableName = 'error_log_' . now()->format('Ymd');
+        if( !Redis::get( $tableName, $tableName ) )
+        {
+            if ( !Schema::hasTable( $tableName ) )
+            {
+                Schema::create( $tableName, function ($table) {
+                    $table->id();
+                    $table->string('error_code',25);
+                    $table->text('error_message');
+                    $table->timestamps();
+                });
+            }
+            Redis::set( $tableName, $tableName);
+            Redis::expire($tableName, 86400);
+        }
+
+        DB::table($tableName)->insert($this->data);
     }
 }
--- app/Jobs/WriteRequestLogJob.php
+++ app/Jobs/WriteRequestLogJob.php
@@ -3,12 +3,13 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Schema;
 
 class WriteRequestLogJob implements ShouldQueue
 {
@@ -29,6 +30,25 @@ class WriteRequestLogJob implements ShouldQueue
      */
     public function handle(): void
     {
-        DB::table('system_log')->insert($this->data);
+        $tableName = 'system_log_' . now()->format('Ymd');
+
+        if (!Redis::get($tableName)) {
+            if (!Schema::hasTable($tableName)) {
+                Schema::create($tableName, function ($table) {
+                    $table->id();
+                    $table->bigInteger('user_id',false)->default(0)->comment('用户ID');
+                    $table->string('path',50)->comment('请求路由地址');
+                    $table->string('method',50)->comment('请求方式');
+                    $table->string('ip',100)->comment('用户IP地址');
+                    $table->text('content')->comment('请求内容');
+                    $table->decimal('response_time', 18, 15)->comment('响应时间');
+                    $table->timestamps();
+                });
+            }
+            Redis::set($tableName, $tableName);
+            Redis::expire($tableName, 86400);
+        }
+        //将请求信息存储到数据库
+        DB::table($tableName)->insert($this->data);
     }
 }
